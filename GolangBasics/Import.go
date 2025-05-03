9776373cbef22ae37dc7227a0fb28bcfdc99e55f
Maddy <142102472+PriestCoder@users.noreply.github.com>
2025-05-03 19:04:34 +0530

    Second Commit

---
+++ GolangBasics/Import.go
@@ -0,0 +1,22 @@
+package main
+
+import ("fmt"
+   "net/http")
+
+func main(){
+
+	fmt.Println("Hello , Maddy ")
+
+	resp , err := http.Get("https://pkg.go.dev/fyne.io/fyne/v2#readme-shipping-the-fyne-toolkit")
+	if err!=nil {
+		fmt.Println("Error :",err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("HTTP Rsponse Status :",resp.Status)
+
+
+
+
+}
---
+++ GolangBasics/Variables.go
@@ -0,0 +1,25 @@
+package GolangBasics
+
+
+import "fmt"
+
+var Caste = "Sharma"
+
+
+func main(){
+	var Name = "Madhav Gaur" ;
+	var num int = 100000000000000 ;
+	GoodName := "Maddy"
+	Caste := "Gaur"
+	
+	fmt.Println(Name,num,GoodName,Caste) ;
+
+	SDE()
+}
+
+
+func SDE(){
+	Goal := "Quantative Trader @Jane Street"
+	fmt.Println(Goal)
+}
+
